5247f55bfb0176d53748d156325f9fd0e993dbd0
Tigerism <julienkmec@Juliens-MacBook-Pro.local>
2017-02-05 11:04:30 -0500

    Updated Event handling

--- resources/modules/Events.lua
+++ resources/modules/Events.lua
@@ -1,10 +1,13 @@
-local Events = {}
-
-local function init(client)
+return function(client)
 	local commands = client.commands
+	
+	client:on("ready",function()
+		p("Logged in as "..client.user.username)
+		client.started = os.time()
+	end)
+	
 	client:on("messageCreate",function(message)
 		commands:newMsg(message)
 	end)
+	
 end
-
-return init
