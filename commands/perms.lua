a0d500ea78b6fb3db5caef4ae1201fc60fc52f82
Tigerism <Tigerism@users.noreply.github.com>
2017-04-11 16:59:41 -0400

    Filename change

--- commands/perms.lua
+++ commands/perms.lua
@@ -2,13 +2,14 @@ return {
 	--Permissions
 	type = "permissions"
 },function(message,args)
-    db:set("guilds/"..message.guild.id,"locale",{locale = args[1]})
-    return ":ok_hand:"
+    
 end,{
     type = "args"
 	--args
 },{
     type = "subcommands"
 	--subcommands
+},{
+    
 }
 
