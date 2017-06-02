f695d628d673c074cd00d1415743a31a7bf479a8
Tigerism <tigerismrblx@gmail.com>
2017-06-02 15:09:11 -0400

    Useless

--- resources/listeners/ready.lua
+++ resources/listeners/ready.lua
@@ -1,7 +1,5 @@
 local function handleReady()
 	print("Logged in as "..client.user.username)
-	local correct = client.shardCount == 1 and "shard" or "shards"
-	client.framework.modules["logger"][1]:sendLog("Master","Started "..client.shardCount.." "..correct..".","8901500")
 end
 
 framework:wrapHandler("ready",handleReady)
