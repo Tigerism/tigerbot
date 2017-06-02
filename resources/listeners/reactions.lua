4e264d17787932dbf27a6fa2a41858903f23525d
Tigerism <tigerismrblx@gmail.com>
2017-06-01 20:51:38 -0400

    Fixes

--- resources/listeners/reactions.lua
+++ resources/listeners/reactions.lua
@@ -5,14 +5,25 @@ local leftArrow = "⬅"
 local rightArrow = "➡"
 local wasteBin = "🗑"
 	
-function reactions:newReactionMenu(oldMessage,msg,fields)
+function reactions:newReactionMenu(oldMessage,msg,fields,options)
+	options = options or {}
 	coroutine.wrap(function()
 		msg:addReaction(info)
 		msg:addReaction(leftArrow)
 		msg:addReaction(rightArrow)
 		msg:addReaction(wasteBin)
 	end)()
-	reactions.menus[msg.id] = {oldMessage=oldMessage,msg=msg,infoEmbed=msg.embed,fields=fields,page=0,emojis={leftArrow,rightArrow}}
+	if options.startingPage then
+		local embed = msg.embed
+		local fields = modules.pagination[1]:toEmbed(options.startingPage,fields,5)
+		if #fields == 0 then
+			--embed.description = embed.description.."\n\n**There's no data to show.**"
+		else
+			embed.fields = fields
+		end
+		msg:setEmbed(embed)
+	end
+	reactions.menus[msg.id] = {oldMessage=oldMessage,msg=msg,infoEmbed=msg.embed,fields=fields,page=(options.startingPage or 0),emojis={leftArrow,rightArrow},options=options}
 end
 
 local function reactionAdd(reaction,user)
@@ -24,30 +35,49 @@ local function reactionAdd(reaction,user)
 	local menu = reactions.menus[message.id]
 	if menu then
 		message:removeReaction(emoji,member)
-		local embed
+		local embed = {}
 		if user.id == menu.oldMessage.author.id then
 			if emoji == leftArrow then
-				local fields = modules.pagination:toEmbed(menu.page-1,menu.fields,5)
+				local fields = modules.pagination[1]:toEmbed(menu.page-1,menu.fields,5)
 				if #fields > 0 then
 					menu.page = menu.page - 1
-					embed = {}
 					embed.fields = fields
 				end
 			elseif emoji == rightArrow then
 				local fields = modules.pagination[1]:toEmbed(menu.page+1,menu.fields,5)
 				if #fields > 0 then
 					menu.page = menu.page + 1
-					embed = {}
 					embed.fields = fields
 				end
 			elseif emoji == info then
+				if menu.options.infoPage then
+					embed = menu.options.infoPage
+				else
+					embed = menu.infoEmbed
+				end
 				menu.page = 0
-				embed = menu.infoEmbed
 			elseif emoji == wasteBin then
 				message:delete()
 				menu.oldMessage:delete()
 			end
-			message:setEmbed(embed)
+			if emoji ~= info then
+				if not embed.fields or #embed.fields == 0  then
+					if embed.description then
+						embed.description = embed.description.."\n\n**There's no data to show.**"
+					else
+						embed.description = "**There's no data to show.**"
+					end
+				else
+					embed.description = menu.options.description or embed.description or ""
+				end
+			end
+			if emoji == leftArrow or emoji == rightArrow then
+				if embed.fields and #embed.fields ~= 0 then
+					message:setEmbed(embed)
+				end
+			elseif emoji == info then
+				message:setEmbed(embed)
+			end
 		end
 	end
 end
