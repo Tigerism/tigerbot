11f3ad149a89339612c8fa5500d81d9236f56c8b
Tigerism <tigerismrblx@gmail.com>
2017-06-03 00:15:26 -0400

    Warn command

---
+++ commands/warn.lua
@@ -0,0 +1,16 @@
+return {
+    description = "warns a member.",
+    args = {{"user","Please specify a user."},{"string","Please state the reason."}},
+    permissions = {roles = {"Bot Commander"}},
+    category = "mod"
+},
+function(message,args,flags)
+    local user = args.myArgs[1]
+    local guild = message.guild
+    respond:success("Successfully warned **"..user.username.."**")
+    modules.logger[1]:newModLog(guild,user,{
+        type = "Warn",
+        reason = args.myArgs[2],
+        mod = message.author.id
+    })
+end
