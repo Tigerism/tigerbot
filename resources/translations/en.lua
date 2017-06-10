825432932a201c4a69511cb4931c65f382a7cd4b
Tigerism <tigerismrblx@gmail.com>
2017-06-10 00:04:09 -0400

    More locales

--- resources/translations/en.lua
+++ resources/translations/en.lua
@@ -1,4 +1,5 @@
 return {
+    bots = "Bots",
     ping = {
         ping = "pinging....",
         pong = "pong ``{{arg1}}ms``",
@@ -6,7 +7,7 @@ return {
     },
     help = {
         embedTitle = "Help for Tiger 2.0",
-        embedDescription = "Hello! I'm Tiger, a bot written in Lua with the Discordia library.\nUse the reaction buttons to filter through the command list.\n\nOfficial links: [Discord Server](https://discord.gg/62qYz8J) | [Invite](https://discordapp.com/oauth2/authorize?&client_id=225079733799485441&scope=bot&permissions=8)\n\nRemember: for most commands, if it requires a prompt, you can skip it with the ``|`` character. For example: ``tiger ban @user | reason | duration``. If a command is restricted for you, you can grant it with ``tiger perms``",
+        embedDescription = "Hello! I'm Tiger, a bot written in Lua with the Discordia library.\nUse the reaction buttons to filter through the command list.\n\nRemember: for most commands, if it requires a prompt, you can skip it with the ``|`` character. For example: ``tiger ban @user | reason | duration``. If a command is restricted for you, you can grant it with ``tiger perms``.\n\nFinally, most commands have what are called **flags.** A flag allows you to further customize a command. Flags start with two dashes and go at the **END** of the command. Flags may contain arguments.\nTo view detailed information on a command, use the ``--help`` flag. For example: ``tiger ban --help`` will show you information on a command. It'll also show you other flags you can use. Example: ``tiger prune 100 --bots --contains test`` will look through 100 messages and prune the ones created by bots that contain the text 'test'.\n\nOfficial links: [Discord Server](https://discord.gg/62qYz8J) | [Invite](https://discordapp.com/oauth2/authorize?&client_id=225079733799485441&scope=bot&permissions=8)\n",
         description = "shows this menu",
     },
     reload = {
@@ -35,6 +36,39 @@ return {
         invalidRoles = "Invalid Roles",
         none = "None to show.",
         autorolesFor = "Autoroles for **{{arg1}}**"
-        
+    },
+    server = {
+        description = "view detailed server information.",
+        largeServer = "Large Server",
+        owner = "Owner",
+        emojis = "Emojis",
+        members = "Members",
+    },
+    invite = {
+        title = "Invite Tiger",
+        botInvite = "Bot Invite",
+        helpInvite = "Help Server Invite",
+        description = "invites Tiger to your server."
+    },
+    user = {
+        description = "view detailed user information."
+    },
+    stats = {
+        description = "shows detailed information about Tiger."
+    },
+    ["8ball"] = {
+        description = "predict the future!"
+    },
+    flipcoin = {
+        description = "flip a coin."
+    },
+    cat = {
+        description = "sends a random picture of a cat."
+    },
+    dog = {
+       description = "sends a random picture of a dog." 
+    },
+    perms = {
+        description = "configure command permissions."
     }
 }
