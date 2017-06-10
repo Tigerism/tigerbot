15bab237aa4ebeedf38846195ef2baf75e115933
Tigerism <tigerismrblx@gmail.com>
2017-06-10 00:04:37 -0400

    Server flags

--- commands/server.lua
+++ commands/server.lua
@@ -1,23 +1,10 @@
 
 return function(message,args,flags)
     local guild = message.guild
+    local options = {}
     
-    local fields = {
-       {name="Owner",value=guild.owner.mentionString,inline=true} 
-    }
-    
-    if guild.emojiCount > 0 then
-        local emojis = {}
-        for emoji in guild.emojis do
-            table.insert(emojis,emoji.string)
-        end
-        table.insert(fields,{name="Emojis",value=(table.concat(emojis,", ").." ("..guild.emojiCount..")"),inline=true})
-    end
     if guild.large then
-        table.insert(fields,{name="Large Server",value=guild.large,inline=true})
-    end
-    if guild.large then
-        table.insert(fields,{name="Large Server",value=guild.large,inline=true})
+        options["large"] = {name=locale("server.largeServer"),value=guild.large,inline=true}
     end
     local members,bots = 0,0
     for member in guild.members do
@@ -27,24 +14,48 @@ return function(message,args,flags)
             members = members + 1
         end
     end
-    table.insert(fields,{name="Members",value="Humans: **"..members.."**\nBots: **"..bots.."**",inline=true})
-    table.insert(fields,{name="Region",value=guild.region,inline=true})
+    options["members"] = {name=locale("server.members"),value="Humans: **"..members.."**\nBots: **"..bots.."**\nTotal: **"..guild.totalMemberCount.."**",inline=true}
+    options["region"] = {name="Region",value=guild.region}
+   
+    if guild.emojiCount > 0 then
+        local emojis = {}
+        for emoji in guild.emojis do
+            table.insert(emojis,emoji.string)
+        end
+        options["emojis"] = {name=locale("server.emojis"),value=(table.concat(emojis,", ").." ("..guild.emojiCount..")")}
+    end
     if guild.roleCount > 0 then
         local roles = {}
         for role in guild.roles do
             table.insert(roles,role.mentionString)
         end
-        table.insert(fields,{name="Roles",value=table.concat(roles,", "),inline=true})
+        options["roles"] = {name="Roles",value=table.concat(roles,", ")}
+    end
+    local fields = {}
+    local embed = {fields={}}
+    if #flags.table >= 1 then
+        for i,v in pairs(flags.array) do
+            local field = options[i]
+            if field then
+                table.insert(embed.fields,field)
+            else
+               table.insert(embed.fields,{name=i,value="none"}) 
+            end
+        end
+    else
+        embed.thumbnail = {url=guild.iconUrl}
+        embed.title = guild.name
+        embed.description = "("..guild.id..")"
+        embed.footer = {text="This server is on Shard "..guild.shardId}
+        for i,v in pairs(options) do
+            table.insert(embed.fields,v)
+        end
+        table.insert(embed.fields,{name=locale("server.owner"),value=guild.owner.mentionString})
     end
     
+    table.reverse(embed.fields)
     message.channel:sendMessage {
-        embed = {
-            thumbnail = {url=guild.iconUrl},
-            title = guild.name,
-            description = "("..guild.id..")",
-            fields = fields,
-            footer = {text="This server is on Shard "..guild.shardId}
-        }
+        embed = embed
     }
     
     
@@ -53,4 +64,6 @@ end,
 {
     description=locale("server.description"),
     category="misc",
+    flags = {owner="shows the server's owner",roles="shows the server's roles",emojis = "shows the server's emojis",members="shows the server's member count",region="shows the server's region",large="shows whether the server is considered 'large'"},
+    examples = {"tiger server","tiger server --emojis","tiger server --roles --members"}
 }
