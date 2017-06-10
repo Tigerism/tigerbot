46819bf0afaa73c9289e7dd62927a435e1b06ab8
Tigerism <tigerismrblx@gmail.com>
2017-06-10 00:03:56 -0400

    Invite locale

--- commands/invite.lua
+++ commands/invite.lua
@@ -1,10 +1,10 @@
 return function(message)
     message.channel:sendMessage {
         embed = {
-            title = "Invite Tiger",
+            title = locale("invite.title"),
             fields = {
-                {name="Bot Invite",value="https://discordapp.com/oauth2/authorize?permissions=85064&scope=bot&client_id=225079733799485441",inline=true},
-                {name="Help Server Invite",value="https://discord.gg/62qYz8J",inline=true},
+                {name=locale("invite.botInvite"),value="https://discordapp.com/oauth2/authorize?permissions=85064&scope=bot&client_id=225079733799485441",inline=true},
+                {name=locale("invite.helpInvite"),value="https://discord.gg/62qYz8J",inline=true},
             }
         }
     }
